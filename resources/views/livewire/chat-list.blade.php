c3d4fe60afc5ac7f1aba14e1d15e73f555acbfef
kebeattie <kebeattie98@hotmail.com>
2025-02-07 16:14:41 +0000

    asda

--- resources/views/livewire/chat-list.blade.php
+++ resources/views/livewire/chat-list.blade.php
@@ -3,13 +3,14 @@
         <a href="{{ route('chat.show', $conversation->id) }}" class="w-full md:w-96">
             @if($conversation->messages()->count() > 0)
                 <div class="bg-gray-700 rounded-lg p-3 flex items-center gap-3 hover:bg-gray-600 transition duration-200">
-
+                    <!-- Image Section -->
                     <div class="bg-white rounded-full p-1">
                         <img width="50" height="50" class="rounded-full"
                              src="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=2048x2048&w=is&k=20&c=6hQNACQQjktni8CxSS_QSPqJv2tycskYmpFGzxv3FNs="
                              alt="User Avatar"/>
                     </div>
 
+                    <!-- Text Section -->
                     <div class="flex flex-col">
                         <span class="text-white font-medium">{{ $conversation->messages()->first()->receiver->name }}</span>
                         <span class="text-sm text-gray-300">{{ $conversation->messages()->first()->message }}</span>
@@ -21,4 +22,3 @@
         </a>
     @endforeach
 </div>
-
